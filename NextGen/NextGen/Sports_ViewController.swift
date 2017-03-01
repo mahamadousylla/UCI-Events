79afb4cbea3a6222d05dafe559d2d85039de5701
mahamadousylla <msylla@uci.edu>
2017-03-01 00:55:21 -0800

    UI near complete. Need to put data on view

--- NextGen/NextGen/Sports_ViewController.swift
+++ NextGen/NextGen/Sports_ViewController.swift
@@ -7,79 +7,90 @@
 //
 
 import UIKit
-//import PlaygroundSupport
 import Foundation
 
-class Sports_ViewController: UIViewController {
+
+
+class Sports_ViewController: UIViewController,
+ UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var tableViewDataSource = [] as Array;
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-//        let url = URL(string: "http://127.0.0.1:5000/m-baskbl")
-//        
-//        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
-//            guard error == nil else {
-//                print(error!)
-//                return
-//            }
-//            guard let data = data else {
-//                print("Data is empty")
-//                return
-//            }
-//            
-//            let json = try! JSONSerialization.jsonObject(with: data, options: [])
-//            print(json)
-//        }
-//        
+        let url = URL(string: "http://127.0.0.1:5000/m-baskbl")
+        
+        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
+                return
+            }
+            
+            let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:AnyObject]
+            
+            let myData = (json["data"] as! NSArray) as Array
+            for dict in myData {
+//                var s = dict["date"] + " " + dict["location"]  + " " + dict["month"]  + " "
+//                + dict["opponent"] + " " + dict["score"] + " " + dict["time"]
+                self.tableViewDataSource.append(dict)
+            }
+            
+//            print(self.tableViewDataSource)
+        }
+        
 //        task.resume()
         
         // Do any additional setup after loading the view.
     }
-
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+//    func returnExampleArray() -> [Array<Any>] {
+    
+    
+//        var tableViewDataSource = ["One", "Two", "Three",
+//                                   "Four", "Five", "Six"];
+    
+    
+        // return the size of the array to tableview
+        func tableView(_ tableView: UITableView,numberOfRowsInSection section: Int) -> Int {
+            print("Here tableView")
+//            print(table_ViewDataSource.count)
+            print(self.tableViewDataSource.count)
+            return self.tableViewDataSource.count
+        }
+    
+        // assign the values in your array variable to cells
+        func tableView(_ tableView: UITableView,
+                       cellForRowAt indexPath: IndexPath) ->
+            UITableViewCell {
+    
+                // get a reference to our storyboard cell
+                print("madeittttt")
+                let cell = self.tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! Sports_TableViewCell
+    
+                //let cell:UITableViewCell=UITableViewCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+    
+                //print(tableViewDataSource)
+                
+//                cell.dataLabel!.text = tableViewDataSource[indexPath.row]
+    
+    
+                return cell;
+        }
+    
 
-//    var tableViewDataSource = ["One", "Two", "Three",
-//                               "Four", "Five", "Six"];
-//
-//    
-//    // return the size of the array to tableview
-//    func tableView(_ tableView: UITableView,
-//                   numberOfRowsInSection section: Int) -> Int {
-//        return tableViewDataSource.count
-//    }
-//    
-//    // assign the values in your array variable to cells
-//    func tableView(_ tableView: UITableView,
-//                   cellForRowAt indexPath: IndexPath) ->
-//        UITableViewCell {
-//            
-//            // get a reference to our storyboard cell
-//            
-//            let cell = self.tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! Sports_TableViewCell
-//            
-//            //let cell:UITableViewCell=UITableViewCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
-//            
-//            // "label!" is the pizzaLabel
-//            cell.dataLabel!.text = tableViewDataSource[indexPath.row]
-//            
-//            
-//            return cell;
-//    }
-//    
-//    // Register when user taps a cell via alert message
-//    func tableView(_ tableView: UITableView,didSelectRowAt indexPath: IndexPath) {
-//        let alert = UIAlertController(title: "Selected Cell:", message: tableViewDataSource[indexPath.row], preferredStyle: UIAlertControllerStyle.alert)
-//        alert.addAction(UIAlertAction(title: "Okay", style:
-//            UIAlertActionStyle.default, handler: nil))
-//        self.present(alert, animated: true, completion:nil)
-//    }
     
 
 
